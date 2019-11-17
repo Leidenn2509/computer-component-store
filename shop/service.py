db17cea209e3edc75069f40fe19be209a3c17e24
Алексей Паульс <aleksey.pauls@mail.ru>
2019-11-17 05:38:17 -0900

    Add 'get_filter_values' function

--- shop/service.py
+++ shop/service.py
@@ -33,3 +33,15 @@ def get_filtered_products(params):
         res.append({"id": r[0], "name": r[1], "price": r[2], "installment_plan": r[3],
                     "warranty_period": r[4], "img_url": r[5], "description": r[6], "cat_num": r[7]})
     return res
+
+
+def get_filter_values(cat_num):
+    cursor = conn.cursor()
+    cursor.execute("""select min(price), max(price), min(warranty_period), max(warranty_period) 
+        from product where cat_num = %(cat_num)s""", {"cat_num": cat_num})
+    records = cursor.fetchall()
+    res = {"minPrice": 0, "maxPrice": 0, "minWarranty": 0, "maxWarranty": 0}
+    if records:
+        res = {"minPrice": records[0][0], "maxPrice": records[0][1], "minWarranty": records[0][2],
+               "maxWarranty": records[0][3]}
+    return res
