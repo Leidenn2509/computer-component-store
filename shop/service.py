d31e3cc1f4242cd56870aaa05c3c6432dd8f8b5c
Алексей Паульс <aleksey.pauls@mail.ru>
2019-11-16 16:42:27 -0900

    Add get_categories function

--- shop/service.py
+++ shop/service.py
@@ -2,4 +2,10 @@ from shop import conn
 
 
 def get_categories():
-    pass
+    cursor = conn.cursor()
+    cursor.execute("""select * from category""")
+    records = cursor.fetchall()
+    res = []
+    for r in sorted(records, key=lambda x: len(x[0])):
+        res.append({"id": r[0], "name": r[1]})
+    return res
