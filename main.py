dace74d5ce38db584549baf38b66221dbe7d89f5
Алексей Паульс <aleksey.pauls@mail.ru>
2019-11-27 15:50:48 -0200

    Add logic for add category

--- main.py
+++ main.py
@@ -29,17 +29,28 @@ def main():
 
 @app.route('/admin', methods=["POST", "GET"])
 def admin():
-    shop_info = get_shop_info()
-    categories = get_categories()
-    node_categories = get_node_categories()
-    leaf_categories = get_leaf_categories()
-    products = get_all_products()
-    return render_template('AdminPage.html',
-                           categories=json.dumps(categories),
-                           node_categories=json.dumps(node_categories),
-                           leaf_categories=json.dumps(leaf_categories),
-                           products=json.dumps(products),
-                           shop_info=json.dumps(shop_info))
+    if request.method == 'POST':
+        data = json.loads(request.data.decode('utf-8').replace('\0', ''))
+        if data["action"] == "addCategory":
+            add_category(data)
+            categories = get_categories()
+            node_categories = get_node_categories()
+            leaf_categories = get_leaf_categories()
+            return make_response(json.dumps({"action": "setCategories", "node_categories": node_categories,
+                                             "leaf_categories": leaf_categories, "categories": categories}), 200,
+                                 {"content_type": "application/json"})
+    else:
+        shop_info = get_shop_info()
+        categories = get_categories()
+        node_categories = get_node_categories()
+        leaf_categories = get_leaf_categories()
+        products = get_all_products()
+        return render_template('AdminPage.html',
+                               categories=json.dumps(categories),
+                               node_categories=json.dumps(node_categories),
+                               leaf_categories=json.dumps(leaf_categories),
+                               products=json.dumps(products),
+                               shop_info=json.dumps(shop_info))
 
 
 if __name__ == '__main__':
